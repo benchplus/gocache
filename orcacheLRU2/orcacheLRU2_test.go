fe2cbf23a65e4dd3b2d4e1adee672839b14d0b98
Orca <orca.zhang@yahoo.com>
2021-12-29 19:45:57 +0800

    :pager:  add GC pause

---
+++ orcacheLRU2/orcacheLRU2_test.go
@@ -0,0 +1,93 @@
+package benchplus
+
+import (
+	"fmt"
+	"os"
+	"runtime/debug"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/benchplus/gocache"
+	"github.com/orca-zhang/orcache"
+)
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	shutdown()
+	os.Exit(code)
+}
+
+func setup() {
+	debug.SetGCPercent(10)
+}
+
+func shutdown() {
+	gocache.PrintGCPause()
+}
+
+func BenchmarkPutInt_orcacheLRU2(b *testing.B) {
+	cache := orcache.NewLRUCache(256, 16, 10*time.Second).LRU2(16)
+	for i := 0; i < b.N; i++ {
+		cache.Put(fmt.Sprint(i), i)
+	}
+}
+
+func BenchmarkGetInt_orcacheLRU2(b *testing.B) {
+	cache := orcache.NewLRUCache(256, 16, 10*time.Second).LRU2(16)
+	cache.Put("0", 0)
+	for i := 0; i < b.N; i++ {
+		cache.Get("0")
+	}
+}
+
+func BenchmarkPut1K_orcacheLRU2(b *testing.B) {
+	cache := orcache.NewLRUCache(256, 16, 10*time.Second).LRU2(16)
+	for i := 0; i < b.N; i++ {
+		cache.Put(fmt.Sprint(i), gocache.Data1K)
+	}
+}
+
+func BenchmarkPut1M_orcacheLRU2(b *testing.B) {
+	cache := orcache.NewLRUCache(256, 16, 10*time.Second).LRU2(16)
+	for i := 0; i < b.N; i++ {
+		cache.Put(fmt.Sprint(i), gocache.Data1M)
+	}
+}
+
+func BenchmarkPutTinyObject_orcacheLRU2(b *testing.B) {
+	cache := orcache.NewLRUCache(256, 16, 10*time.Second).LRU2(16)
+	for i := 0; i < b.N; i++ {
+		cache.Put(fmt.Sprint(i), gocache.UserInfo{})
+	}
+}
+
+func BenchmarkChangeOutAllInt_orcacheLRU2(b *testing.B) {
+	cache := orcache.NewLRUCache(256, 16, 10*time.Second).LRU2(16)
+	for i := 0; i < b.N*1024; i++ {
+		cache.Put(fmt.Sprint(i), i)
+	}
+}
+
+func BenchmarkHeavyRead_orcacheLRU2(b *testing.B) {
+	gocache.GCPause()
+
+	cache := orcache.NewLRUCache(256, 16, 10*time.Second).LRU2(16)
+	for i := 0; i < 1024; i++ {
+		cache.Put(fmt.Sprint(i), i)
+	}
+	var wg sync.WaitGroup
+	for index := 0; index < 10000; index++ {
+		wg.Add(1)
+		go func() {
+			for i := 0; i < 1024; i++ {
+				cache.Get(fmt.Sprint(i))
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	gocache.AddGCPause("HeavyRead")
+}
